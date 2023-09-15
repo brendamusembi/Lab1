3e5f75c12642d150e5216aa162620e54677ad007
brendamusembi <144439011+brendamusembi@users.noreply.github.com>
2023-09-15 15:49:15 +0300

    Add files via upload

---
+++ UsingWhen/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val ch = 'z'
+
+    when (ch) {
+        'a', 'e', 'i', 'o', 'u' -> println("$ch is vowel")
+        else -> println("$ch is consonant")
+    }
+}
