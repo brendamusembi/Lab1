d37d9e9dcb4be45d088d696b22026044705e619c
brendamusembi <144439011+brendamusembi@users.noreply.github.com>
2023-09-15 15:54:28 +0300

    Add files via upload

---
+++ GetUserInputAndDisplayItOnAConsole/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Enter a number:")
+    val stringInput = readLine()!!
+    var integer:Int = stringInput.toInt()
+    println("You entered: $integer")
+
+}
